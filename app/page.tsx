d655ee1fca68576a100143f96d5bc4732429e411
Pol Llinàs <pol.llinas06@gmail.com>
2024-11-30 15:44:20 +0100

    Folders added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <div className="bg-sky-500">asf</div>
   );
 }
---
+++ app/settings/page.tsx
@@ -0,0 +1,5 @@
+export default function drttingd(){
+    return(
+        <h1>tets</h1>
+    )
+}
